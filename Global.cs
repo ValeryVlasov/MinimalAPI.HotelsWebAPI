685a1c5297bb498167af8440e92ea12015e20f20
Valery <valera.vlasov11@mail.ru>
2023-08-27 15:57:13 +0300

    Добавление базы данных Sqlite в проект

---
+++ Global.cs
@@ -0,0 +1,4 @@
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Internal;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.SignalR;
--- Program.cs
+++ Program.cs
@@ -1,25 +1,64 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<HotelDb>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("Sqlite"));
+});
+
 var app = builder.Build();
-var hotels = new List<Hotel>();
 
-app.MapGet("/hotels", () => hotels);
-app.MapGet("/hotels/{id}", (int index) => hotels[index]);
-app.MapPost("/hotels", (Hotel hotel) => hotels.Add(hotel));
-app.MapPut("/hotels", (Hotel hotel) =>
+if (app.Environment.IsDevelopment())
 {
-    var index = hotels.FindIndex(h => h.Id == hotel.Id);
-    if (index < 0) throw new Exception("Not found");
-    hotels[index] = hotel;
-});
-app.MapDelete("/hotels/{id}", (int id) =>
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<HotelDb>();
+    db.Database.EnsureCreated();
+}
+
+
+app.MapGet("/hotels", async (HotelDb db) => await db.Hotels.ToListAsync());
+
+app.MapGet("/hotels/{id}", async (int id, HotelDb db) =>
+    await db.Hotels.FirstOrDefaultAsync(h => h.Id == id) is Hotel hotel
+    ? Results.Ok(hotel)
+    : Results.NotFound());
+
+app.MapPost("/hotels", async ([FromBody] Hotel hotel, HotelDb db) =>
 {
-    var index = hotels.FindIndex(h => h.Id == id);
-    if (index < 0) throw new Exception("Not found");
-    hotels.RemoveAt(index);
+    db.Hotels.Add(hotel);
+    await db.SaveChangesAsync();
+    return Results.Created($"/hotels/{hotel.Id}", hotel);
 });
 
+
+app.MapPut("/hotels", async ([FromBody] Hotel hotel, HotelDb db) =>
+{
+    var hotelFromDb = await db.Hotels.FindAsync(new object[] {hotel.Id});
+    if (hotelFromDb == null) return Results.NotFound();
+    hotelFromDb.Name = hotel.Name;
+    hotelFromDb.Longitude = hotel.Longitude;
+    hotelFromDb.Latitude = hotel.Latitude;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("/hotels/{id}", async (int id, HotelDb db) =>
+{
+    var hotelFromDb = await db.Hotels.FindAsync(new object[] { id });
+    if (hotelFromDb == null) return Results.NotFound();
+    db.Hotels.Remove(hotelFromDb);
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.UseHttpsRedirection();
 
 app.Run();
+
+public class HotelDb : DbContext
+{
+    public HotelDb(DbContextOptions<HotelDb> options) : base(options) {}
+    public DbSet<Hotel> Hotels => Set<Hotel>();
+}
 
 public class Hotel
 {
