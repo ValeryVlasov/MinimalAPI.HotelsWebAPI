ab8c5741ec79dc2435a195d733989fad89dce996
Валерий Власов <valera.vlasov11@mail.ru>
2023-08-28 12:23:37 +0300

    search by coordinates + xml representation

--- Global.cs
+++ Global.cs
@@ -2,3 +2,5 @@ global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Internal;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.SignalR;
+global using System.Xml.Serialization;
+global using System.Reflection;
